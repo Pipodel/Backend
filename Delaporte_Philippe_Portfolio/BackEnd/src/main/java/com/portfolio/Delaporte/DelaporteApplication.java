087756c4a61a42b07142c0a4f508b5798d20886b
Pipodel <98429803+Pipodel@users.noreply.github.com>
2022-10-18 20:35:40 -0300

    Se añade carpeta BackEnd
    2

---
+++ Delaporte_Philippe_Portfolio/BackEnd/src/main/java/com/portfolio/Delaporte/DelaporteApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Delaporte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelaporteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelaporteApplication.class, args);
+	}
+
+}
---
+++ Delaporte_Philippe_Portfolio/BackEnd/src/test/java/com/portfolio/Delaporte/DelaporteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.Delaporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DelaporteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
