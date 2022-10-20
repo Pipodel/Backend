ce499afc6b37da5ebeaa215abb2f1b1964bd7b52
Pipodel <98429803+Pipodel@users.noreply.github.com>
2022-10-19 22:24:51 -0300

    3

--- Delaporte_Philippe_Portfolio/BackEnd/src/main/java/com/portfolio/Delaporte/Controller/PersonaController.java
+++ Delaporte_Philippe_Portfolio/BackEnd/src/main/java/com/portfolio/Delaporte/Controller/PersonaController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin (origins = "http://localhost:4200")
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
--- Delaporte_Philippe_Portfolio/FrontEnd/src/app/app.module.ts
+++ Delaporte_Philippe_Portfolio/FrontEnd/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -13,7 +12,11 @@ import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HysComponent } from './components/hys/hys.component';
 import { ProyectoComponent } from './components/proyecto/proyecto.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -26,14 +29,16 @@ import { HttpClientModule } from '@angular/common/http'
     EducacionComponent,
     HysComponent,
     ProyectoComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
-  //FormsModule,
-  // BrowserAnimationsModule,
+    FormsModule,
+    BrowserAnimationsModule,
     HttpClientModule
   ],
   providers: [],
